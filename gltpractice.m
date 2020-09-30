4630a7f6ae9106ee041bb055487a464689ab0051
Patrick Ogunbufunmi <patrickogunbufunmi@yahoo.com>
2020-09-30 10:58:58 -0400

    Goal Line Projection Files

---
+++ gltpractice.m
@@ -0,0 +1,24 @@
+myFolder = 'C:\Users\pogunbufunmi\Documents\GLT_PHOTOS';
+if ~isdir(myFolder)
+  errorMessage = sprintf('Error: The following folder does not exist:\n%s', myFolder);
+  uiwait(warndlg(errorMessage));
+  return;
+end
+filePattern = fullfile(myFolder, '*.jpg');
+jpegFiles = dir(filePattern);
+for k = 1:length(jpegFiles)
+  baseFileName = jpegFiles(k).name;
+  fullFileName = fullfile(myFolder, baseFileName);
+  fprintf(1, 'Now reading %s\n', fullFileName);
+  imageArray = imread(fullFileName);
+  imshow(imageArray);  % Display image.
+  drawnow; % Force display to update immediately.
+end
+
+f=dir('*.jpg')
+files={f.name}
+for k=1:numel(files)
+  Im{k}=imread(files{k})
+end
+
+rgb2gray(imageArray);
